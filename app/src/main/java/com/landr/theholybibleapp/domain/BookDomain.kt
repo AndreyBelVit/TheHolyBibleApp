700f7bae13e06946316380de16ba1746bf05bc01
Andrey L <jobitdev@gmail.com>
2022-05-15 21:29:10 +0300

    Domain layer for books

--- app/src/main/java/com/landr/theholybibleapp/domain/BookDomain.kt
+++ app/src/main/java/com/landr/theholybibleapp/domain/BookDomain.kt
@@ -5,16 +5,6 @@ import com.landr.theholybibleapp.presentation.BookUi
 
 sealed class BookDomain : Abstract.Object<BookUi, Abstract.Mapper.Empty>(){
 
-    class Success(): BookDomain() {
-        override fun map(mapper: Abstract.Mapper.Empty): BookUi {
-            TODO("Not yet implemented")
-        }
-    }
 
-    class Fail(): BookDomain() {
-        override fun map(mapper: Abstract.Mapper.Empty): BookUi {
-            TODO("Not yet implemented")
-        }
-    }
 
 }
